f7ffb97bc997bdaf9c38200fef9ced7379fea409
Ian <ichellquist@protonmail.com>
2022-10-17 05:37:56 -0400

    First Commit

---
+++ src/main/java/ichellquist/two_ravens/TwoRavensApplication.java
@@ -0,0 +1,13 @@
+package ichellquist.two_ravens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoRavensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoRavensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ichellquist/two_ravens/TwoRavensApplicationTests.java
@@ -0,0 +1,13 @@
+package ichellquist.two_ravens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoRavensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
